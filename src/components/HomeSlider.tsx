079bc0a69f52209ed0ad96483cac42d1ec19ed19
alihossen4 <mdahossen.dev@gmail.com>
2026-05-18 00:25:01 +0600

    style:Home Slider Style changes

--- src/components/HomeSlider.tsx
+++ src/components/HomeSlider.tsx
@@ -5,33 +5,36 @@ import slider1 from "../../public/slider1.jpg"
 import slider2 from "../../public/slider2.jpg"
 import slider3 from "../../public/slider3.jpg"
 import 'swiper/css';
-import { Navigation } from 'swiper/modules';
+import { Navigation, Autoplay } from 'swiper/modules';
 
 
 const HomeSlider = () => {
   return (
-    <div className="home-slider"> 
+    <div className="home-slider py-5"> 
       <div className="container">
         <Swiper
         navigation={true}
-        modules={[Navigation]}
+        autoplay={{ delay: 3000, disableOnInteraction: true }}
+        modules={[Navigation, Autoplay]}
+        className='mySwiper'
     >
       <SwiperSlide>
-        <div className="slider-img">  
-          <Image src={slider1} alt='slider' width={800} height={300} className=' object-cover rounded-lg'/> 
-        </div>
+          <div className="item">
+            <Image src={slider1} alt='slider' width={1200} height={300} className=' w-full h-[600px] important! rounded-lg'/> 
+
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="item">
+              <Image src={slider2} alt='slider' width={1200} height={300} className='w-full h-[600px] important! rounded-lg'/> 
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="item">
+              <Image src={slider3} alt='slider' width={1200} height={300} className='w-full h-[600px] important! rounded-lg'/>
+          </div>
       </SwiperSlide>
-      <SwiperSlide>
-        <div className="slider-img">
-          <Image src={slider2} alt='slider' width={800} height={300} className=' object-cover rounded-lg'/> 
-        </div>
-      </SwiperSlide>
-      <SwiperSlide>
-        <div className="slider-img">
-          <Image src={slider3} alt='slider' width={800} height={300} className=' object-cover rounded-lg'/>
-        </div>
-      </SwiperSlide>
-    </Swiper>
+      </Swiper>
       </div>
     </div>
   );
