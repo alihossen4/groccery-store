9780485b21f13581712ef454376e5bc2cdd48767
alihossen4 <mdahossen.dev@gmail.com>
2026-05-25 00:45:07 +0600

    feat: Footer components updated

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,26 +1,40 @@
-
+"use client"
+import React from "react";
 
 const SignUp = ()=>{
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    const handleSubmit = (e: React.FormEvent<HTMLFormElement>)=>{
+        e.preventDefault();
+        console.log({ name, email, password });
+        setName('');
+        setEmail('');
+        setPassword('');
+    }
     return(
         <section className="w-[30%] mx-auto my-10 py-3 shadow-md">
                     <h1 className="text-3xl text-gray-600 font-[600] text-center">Sign Up to Get Started</h1>
             <div className="w-[500px] mx-auto my-4">
-                <form action="" method="post">
+                <form action="" method="get" onSubmit={handleSubmit}>
                     <label htmlFor="name" className="text-2xl px-5 py-2 mr-10">Name : </label>
-                    <input type="text" name="name" className="border border-gray p-2 my-2 focus:outline-none rounded-md border-2" />
+                    <input type="text" name="name" className="border border-gray p-2 my-2 focus:outline-none rounded-md border-2" value={name}  onChange={(e) => setName(e.target.value)} />
                     <br />
                     <label htmlFor="email" className="text-2xl px-5 py-2 mr-11 ">Email : </label>
-                    <input type="email" name="email" className="border border-gray p-2 my-2 focus:outline-none rounded-md border-2" />
+                    <input type="email" name="email" className="border border-gray p-2 my-2 focus:outline-none rounded-md border-2" value={email} onChange={(e) => setEmail(e.target.value)} />
                     <br />
                     <label htmlFor="password" className="text-2xl px-5 py-2 ">Password : </label>
-                    <input type="password" name="password" className="border border-gray p-2 my-2 focus:outline-none rounded-md border-2" />
+                    <input type="password" name="password" className="border border-gray p-2 my-2 focus:outline-none rounded-md border-2" value={password} onChange={(e) => setPassword(e.target.value)} />
                     <br />
                     <button type="submit" className="text-white  py-2 rounded-md bg-primary hover:bg-green-600 hover:text-white mt-3 ml-5 px-5 text-2xl cursor-pointer">Sign Up</button>
                     <br />
                     <br />
                     <span className="text-xl text-gray-800 ml-5 my-2">Continue With Google</span>
                     <span><a href="">google</a></span>
+
                 </form>
+                <p>{name}, {email}, {password}</p>
             </div>
         </section>
     )
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -40,7 +40,7 @@ const Footer = () =>{
                 </div>
                 <hr className="text-gray-300"/>
                 <div className="lower-footer my-4">
-                    <div className="flex">
+                    <div className="flex justify-between">
                         <div className="contact flex flex-col gap-2 w-[25%]">
                             <h1 className="text-3xl font-[500]">Contact Us</h1>
                             <p>Digital Online Store - Mega Super Store </p>
@@ -49,14 +49,33 @@ const Footer = () =>{
                             <p ><span ><CiChat1 size={50} className="text-primary"/> Online Chat Get Expert Help</span></p>
                         </div>
                         <hr className=""/>
-                        <div className="products">
-
+                        <div className="products w-[15%]">
+                                <h1 className="text-2xl mb-3">Prodcuts</h1>
+                            <ul className="text-[18px] flex flex-col gap-1">
+                                <li>Price drop</li>
+                                <li>New Products</li>
+                                <li>Best sales</li>
+                                <li>Contact us</li>
+                                <li>Sitemap</li>
+                                <li>Stores</li>
+                            </ul>
                         </div>
-                        <div className="company">
-
+                        <div className="company w-[15%]">
+                            <h1 className="text-2xl mb-3">Our Company</h1>
+                            <ul className="text-[18px] flex flex-col gap-1">
+                                <li>Delivery</li>
+                                <li>Legal notice</li>
+                                <li>Terms and conditions of use</li>
+                                <li>About us</li>
+                                <li>Secure Payment</li>
+                                <li>Login</li>
+                            </ul>
                         </div>
-                        <div className="email">
+                        <div className="email w-[40%]">
+                            <h3 className="text-[20px]">Subcribe to newsletter</h3>
+                            <form action="">
 
+                            </form>
                         </div>
                     </div>
                 </div>
