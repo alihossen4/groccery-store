f5d5af2c47e1fd67ced6bed30da4d6fc2f1af5d6
alihossen4 <mdahossen.dev@gmail.com>
2026-06-05 23:27:14 +0600

    feat: added products for listing and sidebar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Link from 'next/link'
 import React from 'react'
 import { FaAngleDown } from 'react-icons/fa6'
@@ -6,15 +7,15 @@ const Navbar = () => {
   return (
     <nav className='py-3 top-0 sticky bg-white z-50 shadow-sm'>
         <div className="container flex items-center justify-between gap-5 top-0 sticky">
-            <Link href={"/"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'>Home</Link>
-            <Link href={"/fruits-vegetables"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'>Fruits & Vegetables</Link>
-            <Link href={"/meats-seafood"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'>Meats & Seafood</Link>
-            <Link href={"/breakfast-dairy"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'>Breakfast & Dairy</Link>
-            <Link href={"/breads-bakery"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'>Breads & Bakery</Link>
-            <Link href={"/beverages"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'>Beverages  </Link>
-            <Link href={"/frozen-foods"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'>Frozen Foods</Link>
-            <Link href={"/biscuits-snacks"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'>Biscuits & Snacks</Link>
-            <Link href={"/groccery-staples"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'>Groccery & Staples</Link>
+            <Link href={"/"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'  prefetch={false}>Home</Link>
+            <Link onClick={(e)=>{e.preventDefault()}} href={"/fruits-vegetables"} className='text-[17px] text-gray-600 font-[600] hover:text-primary' prefetch={false}>Fruits & Vegetables</Link>
+            <Link href={"/meats-seafood"} className='text-[17px] text-gray-600 font-[600] hover:text-primary'  prefetch={false}>Meats & Seafood</Link>
+            <Link href={"/breakfast-dairy"} className='text-[17px] text-gray-600 font-[600] hover:text-primary' prefetch={false}>Breakfast & Dairy</Link>
+            <Link href={"/breads-bakery"} className='text-[17px] text-gray-600 font-[600] hover:text-primary' prefetch={false}>Breads & Bakery</Link>
+            <Link href={"/beverages"} className='text-[17px] text-gray-600 font-[600] hover:text-primary' prefetch={false}>Beverages  </Link>
+            <Link href={"/frozen-foods"} className='text-[17px] text-gray-600 font-[600] hover:text-primary' prefetch={false}>Frozen Foods</Link>
+            <Link href={"/biscuits-snacks"} className='text-[17px] text-gray-600 font-[600] hover:text-primary' prefetch={false}>Biscuits & Snacks</Link>
+            <Link href={"/groccery-staples"} className='text-[17px] text-gray-600 font-[600] hover:text-primary' prefetch={false}>Groccery & Staples</Link>
             <div className="relative group">
               <span className='relative text-[17px] flex text-gray-600 gap-1 font-[600] hover:text-primary'>More <FaAngleDown size={20}/>
 
